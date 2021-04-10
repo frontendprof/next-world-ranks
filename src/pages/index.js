2d8c443cab84eac155018f50a6527cda4d2cac13
frontendprof <https://github.com/frontendprof>
2021-04-10 18:06:59 +0500

    fetching api

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,24 @@ import Head from 'next/head';
 import Layout from "../components/Layout/Layout";
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({countries}) {
+  console.log(countries);
   return(
-    <Layout>main</Layout>
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+    </Layout>
   )
+}
+
+
+export const getStaticProps=async()=>{
+
+  const res=await fetch("https://restcountries.eu/rest/v2/all");
+  const countries=await res.json();
+
+  return{
+    props:{
+      countries,
+    }
+  }
 }
